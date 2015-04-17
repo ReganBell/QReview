a9be35d250bd973717be7f471d22c3eb2fd36513
ReganBell <regan@refresh.io>
2015-04-17 17:13:19 -0400

    Sentiment analysis

--- TextRank.py
+++ TextRank.py
@@ -1,19 +1,20 @@
-'''
+"""
 Regan Bell, Akshay Saini, Matthew Beatty
 CS51 Final Project
 Q Comment Summarization
 4/17/15
-'''
+"""
 
 # Import modules
 import nltk
 
 class TextRank(object):
 
-def __init__(self, comments, windowSize, granularity, partsOfSpeech):
+    def __init__(self, comments, windowSize, granularity, partsOfSpeech):
+        self.candidatePhrases =
 
-def _assignEdgesInComment(self, comment):
+    def _assignedgesincomment(self, comment):
 
-def _calculateTFIDF(self, phrase, comment):
+    def _calculateTFIDF(self, phrase, comment):
 
-def _getTextRankEdgesWeightList(self):
+    def _getTextRankEdgesWeightList(self):
