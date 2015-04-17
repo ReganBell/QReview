b049d1c3e162044368dd56c95c8dbe9c30d1e915
ReganBell <regan@refresh.io>
2015-04-17 17:02:26 -0400

    TextRank signature

--- TextRank.py
+++ TextRank.py
@@ -1,1 +1,19 @@
-__author__ = 'regan'
+'''
+Regan Bell, Akshay Saini, Matthew Beatty
+CS51 Final Project
+Q Comment Summarization
+4/17/15
+'''
+
+# Import modules
+import nltk
+
+class TextRank(object):
+
+def __init__(self, comments, windowSize, granularity, partsOfSpeech):
+
+def _assignEdgesInComment(self, comment):
+
+def _calculateTFIDF(self, phrase, comment):
+
+def _getTextRankEdgesWeightList(self):
